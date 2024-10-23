b16f40fff9836e5b87fda3ccf65304c77cf793b8
Megaman99 <45898864+Megaman99@users.noreply.github.com>
2024-10-23 16:50:51 +0200

    Inizializzazione app

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+import Fastify from 'fastify'
+import firstRoute from './routes/user.js'
+
+const fastify = Fastify({
+  logger: true
+})
+fastify.register(firstRoute)
+
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
