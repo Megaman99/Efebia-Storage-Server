e25e635c35a738f80fcfc4b4f47af6c73a106a4b
Megaman99 <45898864+Megaman99@users.noreply.github.com>
2024-10-24 09:53:46 +0200

    Registrazione con cifratura e salvataggio

--- routes/user.js
+++ routes/user.js
@@ -1,24 +1,72 @@
 'use strict'
 
-async function routes (fastify, options) {
-    // Home
-    fastify.get('/', async (request, reply) => {
-        return { title: 'Home' }
-    })
-    
+import bcrypt from 'bcrypt'
+import fs from 'fs'
+
+async function routes (fastify, options) {    
     // Register
-    fastify.post('/register', async (request, reply) => {
-        return { title: 'Register' }
+    fastify.post('/register', {
+        schema: {
+            body: {
+                type: 'object',
+                required: ['email', 'password'],
+                properties: {
+                    email: {type: 'string', format: 'email'},
+                    password: {type: 'string'}
+                }
+            }
+        }
+    }, async (request, reply) => {
+        const saltRounds = 10;
+        const password = request.body.password;
+        console.log('Password: ', password)
+        const email = request.body.email;
+        console.log('Email: ', email)
+
+        bcrypt.hash(password, saltRounds, function(err, hash) {
+            if(err){
+                reply.send('Problema durante l\'elaborazione della password')
+            }
+            console.log('Hash: ', hash)
+
+            const user = {
+                email: email,
+                password: hash
+            };
+
+            const userData = JSON.stringify(user) + "\n";
+
+            fs.appendFile('./data.txt', userData, err => {
+                if (err) {
+                    return reply.send({message: 'Errore nella scrittura del file'})
+                } else {
+                    reply.send({message: 'File scritto con successo'})
+                }
+            });
+        });
+        reply.send({ message: `Utente registrato correttamente` })
     })
 
     // Login
-    fastify.post('/login', async (request, reply) => {
-        return { title: 'Login' }
+    fastify.post('/login', {
+        schema: {
+            body: {
+                type: 'object',
+                required: ['email', 'password'],
+                properties: {
+                    email: {type: 'string', format: 'email'},
+                    password: {type: 'string'}
+                }
+            }
+        }
+    }, async (request, reply) => {
+
+        reply.send( { title: 'Login' })
     })
 
     // Delete
     fastify.delete('/delete', async (request, reply) => {
-        return { title: 'Delete' }
+        reply.send( { title: 'Delete' })
     })
 }
 
