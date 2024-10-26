3ec39ba7b5beafd96b6902f5418f5a743869d1b7
Megaman99 <45898864+Megaman99@users.noreply.github.com>
2024-10-27 00:24:13 +0200

    Rimossa dipendenza

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import Fastify from 'fastify'
+// import fastifyJwt from '@fastify/jwt'
 import userRoute from './routes/user.js'
 import dataRoute from './routes/data.js'
 
@@ -10,6 +11,7 @@ const fastify = Fastify({
   //   prettyprint: true
   // }
 })
+
 fastify.register(userRoute)
 fastify.register(dataRoute)
 
